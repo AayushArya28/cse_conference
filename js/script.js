770d3ceb4455b6bade2e810d6a5677f4cadac570
Ashutosh Kumar <89670760+Ashutosh9470@users.noreply.github.com>
2025-03-04 00:42:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadFooter() {
   fetch('footer.html')
     .then(response => response.text())
@@ -10,11 +9,51 @@ function loadFooter() {
 
 loadFooter();
 
+document.addEventListener("DOMContentLoaded", function() {
+    const ham = document.getElementById("mobile-menu");
+    const ham1 = document.getElementById("ham-nav-1");
+    if (ham && ham1) {
+        ham.addEventListener("click", function(e) {
+            ham1.classList.toggle("flexbox");
+            ham1.classList.toggle("hidden");
+        });
+    }
 
-const div = document.getElementById("ham1");
-const div2 = document.getElementById("straight");
-div.addEventListener("click", () => {
-    div2.classList.toggle("flexbox");
-    div2.classList.toggle("hidden");
+    const cross = document.getElementById("close");
+    if (cross && ham1) {
+        cross.addEventListener("click", function(e) {
+            ham1.classList.toggle("flexbox");
+            ham1.classList.toggle("hidden");
+        });
+    }
 
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
+
+    document.querySelector('.prev').addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    document.querySelector('.next').addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    setInterval(function() {
+        plusSlides(1);
+    }, 2000); // Change image every 2 seconds
 });
